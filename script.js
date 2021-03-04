eebc2c49fed7cc71e188bfc00de0479c77b5c164
joncoded <joncoded@gmail.com>
2021-03-04 01:30:51 -0500

    making first version

---
+++ script.js
@@ -0,0 +1,26 @@
+const url = 'https://random-data-api.com/api/beer/random_beer?size=1';
+
+async function getBeer() {
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  let output = '<ul>';
+  
+  data.forEach((beer) => {
+    // console.log(beer)           
+    output += `
+      <li>
+        <a href='https://www.beeradvocate.com/search/?q=${beer.name}' target='_blank'>${beer.name}</a>
+      </li>`
+  })
+
+  output += '</ul>';
+
+  document.getElementById('output').innerHTML = output;
+
+}
+
+getBeer();
+
+document.getElementById('getbeer').addEventListener('click', getBeer);
